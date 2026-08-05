b7c16c7a29016a5a8218150c263db8739c8b1284
Nelluri-Yoffi <nelluriyoffi@gmail.com>
2026-08-05 12:23:54 +0530

    vectors

---
+++ Yoffi_java/MyVectors.java
@@ -0,0 +1,16 @@
+
+import java.util.Vector;
+class MyVectors{
+    public static void main(String[] args) {
+        Vector myVector= new Vector(5,3);
+        myVector.add("Harshini");
+        myVector.add("Amu");
+        myVector.add("Aafreen");
+        
+        myVector.add("Harshini");
+        myVector.add("Amu");
+        myVector.add("Aafreen");
+        System.out.println(myVector.size());
+        System.out.println(myVector.capacity());
+    }
+}
